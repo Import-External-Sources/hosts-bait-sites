92a92762e632ee2d7f857c5f8cc7efc23f30df96
spirillen <44526987+spirillen@users.noreply.github.com>
2019-10-03 18:11:59 +0200

    Update FinalCommit.sh

--- dev-tools/FinalCommit.sh
+++ dev-tools/FinalCommit.sh
@@ -23,12 +23,14 @@
 cat ${TRAVIS_BUILD_DIR}/dev-tools/output/domains/ACTIVE/list | grep -v "^$" | grep -v "^#" > tempdomains.txt
 mv tempdomains.txt ${TRAVIS_BUILD_DIR}/PULL_REQUESTS/domains.txt
 
+mv ${TRAVIS_BUILD_DIR}/dev-tools/output/hosts/ACTIVE/hosts ${TRAVIS_BUILD_DIR}/hosts
+
 # ***************************************************************************
 # Generate our host file and update README with build and version information
 # ***************************************************************************
 
 #bash ${TRAVIS_BUILD_DIR}/dev-tools/UpdateReadme.sh
-bash ${TRAVIS_BUILD_DIR}/dev-tools/GenerateHostsFile.sh
+#bash ${TRAVIS_BUILD_DIR}/dev-tools/GenerateHostsFile.sh
 
 # *************************************************************
 # Travis now moves to the before_deploy: section of .travis.yml
